5428760b0e21d0a5f36128cd9df3e42072db6a1f
Ali Nazari <alinazarigh.official@gmail.com>
2026-08-18 15:14:04 +0330

    exercise foundations/08_removeFromArray solved

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...lookup) {
+    return arr.filter(item => !lookup.includes(item));
 };
 
 // Do not edit below this line

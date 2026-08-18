988d19fcfcce8df909426c0a1782e2307dfa494f
Ali Nazari <alinazarigh.official@gmail.com>
2026-08-18 15:33:23 +0330

    exercise foundations/11_tempConversion solved

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+function f(x) {
+  return Math.round(x * 10) / 10;
+}
+
+const convertToCelsius = function (x) {
+  return f((x - 32) * (5 / 9));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (x) {
+  return f(x * (9 / 5) + 32);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };

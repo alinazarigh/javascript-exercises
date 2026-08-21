9d226c8558aa1072604903b407b283932e2ad31c
Ali Nazari <alinazarigh.official@gmail.com>
2026-08-21 22:45:33 +0330

    exercise foundations/13_palindromes solved

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    let cleanedString = "";
+    for (const ch of string) {
+        if (
+            ("a" <= ch && ch <= "z") ||
+            ("0" <= ch && ch <= "9")
+        ) {
+            cleanedString += ch;
+        }
+    }
+    return cleanedString === cleanedString.split("").reverse().join("");
 };
 
 // Do not edit below this line

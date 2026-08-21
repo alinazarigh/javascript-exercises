b8eeb5c68636ed7c06a722d62f117b35b3b95d2c
Ali Nazari <alinazarigh.official@gmail.com>
2026-08-21 22:58:59 +0330

    exercise foundations/14_fibonacci solved

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    number = parseInt(number);
+    if (number < 0) return "OOPS";
+    if (number === 0) return 0;
+    let current = 1;
+    let prev = 1;
+    for (let i = 0; i < number - 1; ++i) {
+        const temp = current;
+        current += prev;
+        prev = temp;
+    }
+    return prev;
 };
 
 // Do not edit below this line

f55c65e686ebff56ae36f4cac7d214f156075cad
Ali Nazari <alinazarigh.official@gmail.com>
2026-08-21 23:12:07 +0330

    exercise foundations/16_findTheOldest solved

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
+const getAge = function(person) {
+    person.yearOfDeath ??= new Date().getFullYear();
+    return person.yearOfDeath - person.yearOfBirth;
+};
 
+const findTheOldest = function(people) {
+    return people.sort((a, b) => getAge(b) - getAge(a))[0];
 };
 
 // Do not edit below this line
